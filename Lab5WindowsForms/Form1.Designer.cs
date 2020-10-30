ef43ac6d37f5a4072e830bdd675a6491af3f4a78
Philip Alderstig <alderstig@hotmail.com>
2020-10-30 14:49:20 +0100

    Add project files.

---
+++ Lab5WindowsForms/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Lab5WindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxoutput = new System.Windows.Forms.TextBox();
+            this.buttonExtract = new System.Windows.Forms.Button();
+            this.labelLinkCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSave.Enabled = false;
+            this.buttonSave.Location = new System.Drawing.Point(1159, 1001);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(161, 49);
+            this.buttonSave.TabIndex = 0;
+            this.buttonSave.Text = "Save images";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(12, 26);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(1152, 31);
+            this.textBoxInput.TabIndex = 1;
+            this.textBoxInput.Text = "Input link i.e \'google.com\'";
+            this.textBoxInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxInput_KeyDown);
+            // 
+            // textBoxoutput
+            // 
+            this.textBoxoutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxoutput.Location = new System.Drawing.Point(12, 71);
+            this.textBoxoutput.Multiline = true;
+            this.textBoxoutput.Name = "textBoxoutput";
+            this.textBoxoutput.ReadOnly = true;
+            this.textBoxoutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxoutput.Size = new System.Drawing.Size(1308, 898);
+            this.textBoxoutput.TabIndex = 2;
+            // 
+            // buttonExtract
+            // 
+            this.buttonExtract.Location = new System.Drawing.Point(1170, 26);
+            this.buttonExtract.Name = "buttonExtract";
+            this.buttonExtract.Size = new System.Drawing.Size(161, 39);
+            this.buttonExtract.TabIndex = 3;
+            this.buttonExtract.Text = "Extract";
+            this.buttonExtract.UseVisualStyleBackColor = true;
+            this.buttonExtract.Click += new System.EventHandler(this.buttonExtract_Click);
+            // 
+            // labelLinkCount
+            // 
+            this.labelLinkCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelLinkCount.AutoSize = true;
+            this.labelLinkCount.Location = new System.Drawing.Point(7, 1001);
+            this.labelLinkCount.Name = "labelLinkCount";
+            this.labelLinkCount.Size = new System.Drawing.Size(408, 25);
+            this.labelLinkCount.TabIndex = 4;
+            this.labelLinkCount.Text = "Number of images found in input link: N/A";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1352, 1062);
+            this.Controls.Add(this.labelLinkCount);
+            this.Controls.Add(this.buttonExtract);
+            this.Controls.Add(this.textBoxoutput);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.buttonSave);
+            this.MinimumSize = new System.Drawing.Size(1378, 1133);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ImageScraper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxoutput;
+        private System.Windows.Forms.Button buttonExtract;
+        private System.Windows.Forms.Label labelLinkCount;
+    }
+}
+
---
+++ Lab5WindowsForms/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public HttpClient Client { get; set; }
+        public MatchCollection Matches { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void buttonSave_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    Dictionary<Task<byte[]>, string> images = new Dictionary<Task<byte[]>, string>();
+                    string[] links = textBoxoutput.Text.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                    for (int i = 0; i < links.Length; i++)
+                    {
+                        string match = links[i];
+
+                        images.Add(Client.GetByteArrayAsync(match), match);
+                    }
+
+                    int count = 1;
+
+                    while (images.Count > 0)
+                    {
+                        Task<byte[]> completedTask = await Task.WhenAny(images.Keys);
+                        string checkType = "";
+                        string fileType = "";
+                        
+
+                        foreach (KeyValuePair<Task<byte[]>, string> kvp in images)
+                        {
+                            if (kvp.Key == completedTask)
+                            {
+                                checkType = kvp.Value;
+                                break;
+                            }  
+                        }
+                        
+                        if (checkType.Contains("png"))
+                        {
+                            fileType = "png";
+                        }
+                        else if (checkType.Contains("bmp"))
+                        {
+                            fileType = "bmp";
+                        }
+                        else if (checkType.Contains("gif"))
+                        {
+                            fileType = "gif";
+                        }
+                        else if (checkType.Contains("jpg"))
+                        {
+                            fileType = "jpg";
+                        }
+                        else if (checkType.Contains("jpeg"))
+                        {
+                            fileType = "jpeg";
+                        }
+
+                        string fileName = $@"{dialog.SelectedPath}\Image{count}.{fileType}";
+                        var fs = new FileStream(fileName, FileMode.Create);
+
+                        using (BinaryWriter binWriter = new BinaryWriter(fs))
+                        {
+                            foreach (byte data in completedTask.Result)
+                            {
+                                binWriter.Write(data);
+                            }
+                        }
+
+                        images.Remove(completedTask);
+
+                        count++;
+                    }
+
+                    MessageBox.Show("Download is complete");
+
+                    textBoxoutput.Clear();
+                    textBoxInput.Text = "Input link i.e 'google.com'";
+                    labelLinkCount.Text = "Number of images found in input link: N/A";
+                    buttonSave.Enabled = false;
+                    buttonExtract.Enabled = true;
+                    textBoxInput.Enabled = true;
+                }
+            }
+        }
+
+        private async void buttonExtract_Click(object sender, EventArgs e)
+        {
+            int pngImages = 0;
+            Client = new HttpClient();
+            string inputCorrectForm = $"http://{textBoxInput.Text.ToLower()}";
+            textBoxInput.Text = inputCorrectForm;
+
+            Task<string> download = Client.GetStringAsync(inputCorrectForm);
+            Regex reSrc = new Regex("(?<=<img.+?data-src=)[\"'](.+?)[\"']", RegexOptions.IgnoreCase);
+
+            await download;
+
+            Matches = reSrc.Matches(download.Result);
+
+            for ( int i = 0; i < Matches.Count; i++)
+            {
+                string match = Matches[i].Value.Trim('"');
+
+                if (!string.IsNullOrEmpty(match))
+                {
+                    textBoxoutput.AppendText($"{inputCorrectForm}{match} {Environment.NewLine}");
+
+                    pngImages++;
+                }
+            }
+
+            labelLinkCount.Text = $"Number of images found in input link: {pngImages}";
+            buttonSave.Enabled = true;
+            buttonExtract.Enabled = false;
+            textBoxInput.Enabled = false;
+        }
+
+        private void textBoxInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) buttonExtract.PerformClick();
+        }
+    }
+}
